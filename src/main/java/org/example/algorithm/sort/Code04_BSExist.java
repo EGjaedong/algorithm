043a82365046e8cfba93ad94b562891dcecd480e
zhiheng he <hzh_innovation@163.com>
2024-12-19 22:23:38 +0800

    [zhiheng he] feat : add bs exist and near left algorithm

---
+++ src/main/java/org/example/algorithm/sort/Code04_BSExist.java
@@ -0,0 +1,22 @@
+package org.example.algorithm.sort;
+
+public class Code04_BSExist {
+
+    public static boolean exist(int[] sortedArr, int target) {
+        if (sortedArr == null || sortedArr.length == 0) {
+            return false;
+        }
+        int L = 0, R = sortedArr.length - 1;
+        while (L < R) {
+            int mid = L + ((R - L) >> 1);
+            if (sortedArr[mid] == target) {
+                return true;
+            } else if (sortedArr[mid] < target) {
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+        return sortedArr[L] == target;
+    }
+}
