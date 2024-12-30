9282bec92f40a6cc05701ac07baa8c3bffd0c64c
zhihenghe <hzh_innovation@163.com>
2024-12-30 17:21:23 +0800

    [zhiheng he] feat : check is sub tree use dfs

--- src/main/java/org/example/algorithm/class05/Code10_SerializeAndReconstructTree.java
+++ src/main/java/org/example/algorithm/class05/Code10_SerializeAndReconstructTree.java
@@ -69,4 +69,51 @@ public class Code10_SerializeAndReconstructTree {
         }
         return false;
     }
+
+    // 同步遍历方式
+    public static boolean isSubTree2(Node root, Node subRoot) {
+        return dfs(root, subRoot);
+    }
+
+    public static boolean dfs(Node root, Node subRoot) {
+        if (root == null) {
+            return false;
+        }
+        return check(root, subRoot) ||
+                dfs(root.left, subRoot) ||
+                dfs(root.right, subRoot);
+    }
+
+    private static boolean check(Node root, Node subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null || subRoot == null) {
+            return false;
+        }
+        if (root.value == subRoot.value) {
+            return check(root.left, subRoot.left) && check(root.right, subRoot.right);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node(3);
+        root.left = new Node(4);
+        root.left.left = new Node(1);
+        root.left.right = new Node(2);
+        root.right = new Node(5);
+        Node subRoot = new Node(4);
+        subRoot.left = new Node(1);
+        subRoot.right = new Node(2);
+        long start1 = System.currentTimeMillis();
+        System.out.println(isSubTree(root, subRoot));
+        long end1 = System.currentTimeMillis();
+        System.out.println("cost time: " + (end1 - start1) + "ms");
+        long start2 = System.currentTimeMillis();
+        System.out.println(isSubTree2(root, subRoot));
+        long end2 = System.currentTimeMillis();
+        System.out.println("cost time: " + (end2 - start2) + "ms");
+    }
 }
